e1832d5867e2c8888b66c24c26b866a13c59893f
Jean <bocchijdan@gmail.com>
2025-06-04 20:57:01 -0500

    feat

---
+++ lib/views/pokemon_view.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class PokemonView extends StatelessWidget {
+  const PokemonView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    
+    return Scaffold(body: Center(child: Text('Pokemon view')));
+
+  }
+}
